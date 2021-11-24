437e4d7de0f295110fadde51f5e04aea30402301
Saeeeae <vnv73@naver.com>
2021-11-24 17:47:21 +0900

    change dataset, preprocess

--- dataset.py
+++ dataset.py
@@ -8,55 +8,44 @@ import matplotlib.pyplot as plt
 import glob, os
 import numpy as np
 
-class GaussianBlur(object):
-    """blur a single image on CPU"""
+train_transform = T.Compose([
+                    T.Resize((250,250)),
+                    T.RandomResizedCrop(224),
+                    T.RandomApply([
+                            T.ColorJitter(0.5, 0.5, 0.5)
+                            ], p=0.8),
+                    T.RandomGrayscale(p=0.2),
+                    T.ToTensor(),
+                    T.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+    ])
 
-    def __init__(self, kernel_size):
-        radias = kernel_size // 2
-        kernel_size = radias * 2 + 1
-        self.blur_h = nn.Conv2d(3, 3, kernel_size=(kernel_size, 1),
-                                stride=1, padding=0, bias=False, groups=3)
-        self.blur_v = nn.Conv2d(3, 3, kernel_size=(1, kernel_size),
-                                stride=1, padding=0, bias=False, groups=3)
-        self.k = kernel_size
-        self.r = radias
+train_transform = T.Compose(
+    [
+        T.Resize((256,256)),      
+        T.RandomHorizontalFlip(),
+        T.RandomResizedCrop(size=224),
+        T.RandomApply([T.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)], p=0.8),
+        T.RandomGrayscale(p=0.2),
+        T.GaussianBlur(kernel_size=9),
+        T.ToTensor(),
+        T.Normalize((0.5,), (0.5,)),
+    ]
+)
 
-        self.blur = nn.Sequential(
-            nn.ReflectionPad2d(radias),
-            self.blur_h,
-            self.blur_v
-        )
-
-        self.pil_to_tensor = T.ToTensor()
-        self.tensor_to_pil = T.ToPILImage()
-
-    def __call__(self, img):
-        img = self.pil_to_tensor(img).unsqueeze(0)
-
-        sigma = np.random.uniform(0.1, 2.0)
-        x = np.arange(-self.r, self.r + 1)
-        x = np.exp(-np.power(x, 2) / (2 * sigma * sigma))
-        x = x / x.sum()
-        x = torch.from_numpy(x).view(1, -1).repeat(3, 1)
-
-        self.blur_h.weight.data.copy_(x.view(3, 1, self.k, 1))
-        self.blur_v.weight.data.copy_(x.view(3, 1, 1, self.k))
-
-        with torch.no_grad():
-            img = self.blur(img)
-            img = img.squeeze()
-
-        img = self.tensor_to_pil(img)
-
-        return img
-
+test_transform = T.Compose(
+    [
+        T.Resize((256,256)),      
+        T.ToTensor(),
+        T.Normalize((0.5,), (0.5,)),
+    ]
+)
 
 class MedDataset(Dataset):
 
   def __init__(self, list_imgs, train=True ,transform=None):
 
     self.list_imgs = list_imgs
-    self.labels = np.asarray([-1]*len(self.list_imgs))
+    self.labels = np.asarray([-1]*len(self.list_imgs))  # if want idx value : np.asarray()
     self. transform = transform
 
   def __len__(self):
@@ -69,6 +58,7 @@ class MedDataset(Dataset):
 
     img_path = self.list_imgs[idx]
     target = self.labels
+
     img = Image.open(img_path)
     
     if self.transform is not None:
@@ -99,10 +89,12 @@ class Loader(object):
     def get_dataset(dataset, file_path, train_transform, test_transform):
 
         # Training and Validation datasets
-        train_dataset = dataset(file_path, train=True,
+        train_dt = glob.glob(os.path.join(file_path,'train/*.jpg'))
+        test_dt = glob.glob(os.path.join(file_path,'test/*.jpg'))
+        train_dataset = dataset(train_dt, train=True,
                                 transform=train_transform)
 
-        test_dataset = dataset(file_path, train=False,
+        test_dataset = dataset(test_dt, train=False,
                                transform=test_transform)
 
         return train_dataset, test_dataset
@@ -110,32 +102,11 @@ class Loader(object):
 
 def main():
 
-    train_transform = T.Compose([
-                    T.Resize((250,250)),
-                    T.RandomResizedCrop(224),
-                    T.RandomApply([
-                            T.ColorJitter(0.5, 0.5, 0.5)
-                            ], p=0.8),
-                    T.RandomGrayscale(p=0.2),
-                    T.ToTensor(),
-                    T.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
-    ])
-    train_transform = T.Compose([
-                    T.Resize((250,250)),
-                    T.RandomResizedCrop(224),
-                    T.RandomApply([
-                            T.ColorJitter(0.5, 0.5, 0.5)
-                            ], p=0.8),
-                    T.RandomGrayscale(p=0.2),
-                    T.ToTensor(),
-                    T.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
-    ])
-    data_dir = glob.glob("C:/Users/SINGKU/Desktop/data/*.jpg")
-    # data_dir = glob.glob("./data/*.jpg")
+    data_dir = "./data/pre_train/"
     use_cuda = "cuda:0" if torch.cuda.is_available() else 'cpu'
     print(use_cuda)
 
-    loader = Loader(data_dir, 4 , train_transform, train_transform, use_cuda)
+    loader = Loader(data_dir, 4 , train_transform, test_transform, use_cuda)
     train_loader = loader.train_loader
     test_loader = loader.test_loader
 
@@ -145,3 +116,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
