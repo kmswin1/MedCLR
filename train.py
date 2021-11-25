9edf36c8a01e8e348ff910322dba5e9b39fab3cb
lucas.ai <lucas.ai@kakaoenterprise.com>
2021-11-25 10:33:54 +0900

    add code

--- train.py
+++ train.py
@@ -1,11 +1,19 @@
 import torchvision.models as models
 import torch
 import torch.nn as nn
-from torch.utils.data import dataset, dataloader
+from torch.utils.data import dataloader
+from dataset import MedDataset
 
 class Trainer:
     def __init__(self):
         self.model = models.resnet18(pretrained=True)
         self.loss = nn.CrossEntropyLoss()
 
-    def train(self):
+    def train(self, batch_data):
+
+    def test(self, batch_data):
+
+
+if __name__ == '__main__':
+    trainer = Trainer()
+    dataset =
