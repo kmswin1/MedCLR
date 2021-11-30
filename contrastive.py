11842cb2565c023a7413d024d9396e1798409282
lucas.ai <lucas.ai@kakaoenterprise.com>
2021-11-30 17:24:34 +0900

    debugging

--- contrastive.py
+++ contrastive.py
@@ -6,13 +6,48 @@ from torch.utils.data import dataset, dataloader
 import logging
 import os
 import sys
-
+from dataset import MedDataset, Loader
 import torch
 import torch.nn.functional as F
 from torch.cuda.amp import GradScaler, autocast
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 from utils import save_config_file, accuracy, save_checkpoint
+from model import ResNetSimCLR
+from utils import EarlyStopping
+from torchvision import transforms as T
+
+train_transform = T.Compose([
+                    T.Resize((250,250)),
+                    T.RandomResizedCrop(224),
+                    T.RandomApply([
+                            T.ColorJitter(0.5, 0.5, 0.5)
+                            ], p=0.8),
+                    T.RandomGrayscale(p=0.2),
+                    T.ToTensor(),
+                    T.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+    ])
+
+train_transform = T.Compose(
+    [
+        T.Resize((256,256)),
+        T.RandomHorizontalFlip(),
+        T.RandomResizedCrop(size=224),
+        T.RandomApply([T.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)], p=0.8),
+        T.RandomGrayscale(p=0.2),
+        T.GaussianBlur(kernel_size=9),
+        T.ToTensor(),
+        T.Normalize((0.5,), (0.5,)),
+    ]
+)
+
+test_transform = T.Compose(
+    [
+        T.Resize((256,256)),
+        T.ToTensor(),
+        T.Normalize((0.5,), (0.5,)),
+    ]
+)
 
 torch.manual_seed(0)
 
@@ -20,19 +55,18 @@ torch.manual_seed(0)
 class SimCLR(object):
 
     def __init__(self, *args, **kwargs):
-        self.args = kwargs['args']
-        self.model = kwargs['model'].to(self.args.device)
-        self.optimizer = kwargs['optimizer']
-        self.scheduler = kwargs['scheduler']
+        self.temperature = 0.2
+        self.model = ResNetSimCLR(4).to('cuda:0')
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
         self.writer = SummaryWriter()
         logging.basicConfig(filename=os.path.join(self.writer.log_dir, 'training.log'), level=logging.DEBUG)
-        self.criterion = torch.nn.CrossEntropyLoss().to(self.args.device)
+        self.criterion = torch.nn.CrossEntropyLoss().to('cuda:0')
 
     def info_nce_loss(self, features):
 
-        labels = torch.cat([torch.arange(self.args.batch_size) for i in range(self.args.n_views)], dim=0)
+        labels = torch.cat([torch.arange(128) for i in range(2)], dim=0)
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
-        labels = labels.to(self.args.device)
+        labels = labels.to('cuda:0')
 
         features = F.normalize(features, dim=1)
 
@@ -42,7 +76,7 @@ class SimCLR(object):
         # assert similarity_matrix.shape == labels.shape
 
         # discard the main diagonal from both: labels and similarities matrix
-        mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.args.device)
+        mask = torch.eye(labels.shape[0], dtype=torch.bool).to('cuda:0')
         labels = labels[~mask].view(labels.shape[0], -1)
         similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
         # assert similarity_matrix.shape == labels.shape
@@ -54,61 +88,76 @@ class SimCLR(object):
         negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
 
         logits = torch.cat([positives, negatives], dim=1)
-        labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.args.device)
+        labels = torch.zeros(logits.shape[0], dtype=torch.long).to('cuda:0')
 
-        logits = logits / self.args.temperature
+        logits = logits / self.temperature
         return logits, labels
 
     def train(self, train_loader):
 
-        scaler = GradScaler(enabled=self.args.fp16_precision)
-
         # save config file
-        save_config_file(self.writer.log_dir, self.args)
-
         n_iter = 0
-        logging.info(f"Start SimCLR training for {self.args.epochs} epochs.")
-        logging.info(f"Training with gpu: {self.args.disable_cuda}.")
 
-        for epoch_counter in range(self.args.epochs):
+        for epoch_counter in range(100):
             for images, _ in tqdm(train_loader):
                 images = torch.cat(images, dim=0)
 
-                images = images.to(self.args.device)
+                images = images.to('cuda:0')
 
-                with autocast(enabled=self.args.fp16_precision):
-                    features = self.model(images)
-                    logits, labels = self.info_nce_loss(features)
-                    loss = self.criterion(logits, labels)
+                features = self.model(images)
+                logits, labels = self.info_nce_loss(features)
+                loss = self.criterion(logits, labels)
 
                 self.optimizer.zero_grad()
 
-                scaler.scale(loss).backward()
+                loss.backward()
 
-                scaler.step(self.optimizer)
-                scaler.update()
+                self.optimizer.step()
 
-                if n_iter % self.args.log_every_n_steps == 0:
-                    top1, top5 = accuracy(logits, labels, topk=(1, 5))
-                    self.writer.add_scalar('loss', loss, global_step=n_iter)
-                    self.writer.add_scalar('acc/top1', top1[0], global_step=n_iter)
-                    self.writer.add_scalar('acc/top5', top5[0], global_step=n_iter)
-                    self.writer.add_scalar('learning_rate', self.scheduler.get_lr()[0], global_step=n_iter)
+if __name__ == '__main__':
+    trainer = SimCLR(4)
+    early_stopping = EarlyStopping()
+    data_dir = "./data/"
+    use_cuda = "cuda:0" if torch.cuda.is_available() else 'cpu'
+    print(use_cuda)
+    optimal_accuracy = 0
 
-                n_iter += 1
+    loader = Loader(data_dir, 128, train_transform, test_transform, use_cuda)
+    train_loader = loader.train_loader
+    test_loader = loader.test_loader
+    with open('log.txt', 'w') as f:
+        for epoch in range(1, 100):
+            tot = 0
+            loss = 0
+            for i, batch_data in enumerate(train_loader):
+                loss += trainer.train(batch_data)
+                tot += batch_data[1].size(0)
 
-            # warmup for the first 10 epochs
-            if epoch_counter >= 10:
-                self.scheduler.step()
-            logging.debug(f"Epoch: {epoch_counter}\tLoss: {loss}\tTop1 accuracy: {top1[0]}")
+            print ("train loss : " + str(loss))
+            f.write("train loss : " + str(loss) + '\n')
 
-        logging.info("Training has finished.")
-        # save model checkpoints
-        checkpoint_name = 'checkpoint_{:04d}.pth.tar'.format(self.args.epochs)
-        save_checkpoint({
-            'epoch': self.args.epochs,
-            'arch': self.args.arch,
-            'state_dict': self.model.state_dict(),
-            'optimizer': self.optimizer.state_dict(),
-        }, is_best=False, filename=os.path.join(self.writer.log_dir, checkpoint_name))
-        logging.info(f"Model checkpoint and metadata has been saved at {self.writer.log_dir}.")
+            #tot = 0
+            #loss = 0
+            #for i, batch_data in enumerate(test_loader):
+            #    valid_loss, accuracy = trainer.test(batch_data)
+            #    loss += valid_loss
+            #    tot += batch_data[1].size(0)
+
+            #print("valid loss : " + str(loss))
+            #print("Accuracy : "+ str(accuracy))
+
+            #f.write("valid loss : " + str(loss) + '\n')
+            #f.write("Accuracy : " + str(accuracy) + '\n')
+
+
+
+
+            #if accuracy > optimal_accuracy:
+            #    optimal_accuracy = accuracy
+            #    print ("model saved")
+            #    torch.save(trainer.model, 'model.pt')
+
+            if early_stopping(loss):
+                print ("early stopped ...")
+                if accuracy > optimal_accuracy:
+                    torch.save(trainer.model, 'model.pt')
