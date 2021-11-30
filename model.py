c4510c71d7bf5ae2f635f4f2c61a7959aee79aab
lucas.ai <lucas.ai@kakaoenterprise.com>
2021-11-30 17:27:29 +0900

    debugging

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ class ResNetSimCLR(nn.Module):
 
     def __init__(self, out_dim):
         super(ResNetSimCLR, self).__init__()
-        models.resnet18(pretrained=False, num_classes=out_dim)
+        self.backbone = models.resnet18(pretrained=False, num_classes=out_dim)
         dim_mlp = self.backbone.fc.in_features
 
         # add mlp projection head
